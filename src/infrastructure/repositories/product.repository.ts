28a362bb07691993270dfb7af75288f1d41dbb71
Isard Bergonyo <isard.bergonyo@gmail.com>
2022-06-29 14:54:56 +0200

    urls

--- src/infrastructure/repositories/product.repository.ts
+++ src/infrastructure/repositories/product.repository.ts
@@ -1,27 +1,14 @@
+// product.repository.ts
+
 import { http } from '../plugins/http/http'
 import { ProductDTO } from '../../infrastructure/dto/ProductDTO'
 import { Product } from '../../domain/models/Product'
+import { getProducts } from '../urls'
 
 export const productRepository = {
     getProducts: async () => {
-        //const products = await http.get<ProductDTO[]>('http://localhost:3001/products')
+        const products = await http.get<ProductDTO[]>(getProducts)
 
-        const products:ProductDTO[]  = [
-            {
-                product_id: "1",
-                title: "test",
-                description: "test description",
-                price: "99",
-            },
-            {
-                product_id: "2",
-                title: "test2",
-                description: "test2 description",
-                price: "199",
-            }
-        ]
-
-        // we can extract this transform to a function inside this file to be reused by different methods
         return products.map((productDto): Product => ({
             id: productDto.product_id,
             title: productDto.title,
@@ -29,3 +16,5 @@ export const productRepository = {
         }))
     }
 }
+
+
